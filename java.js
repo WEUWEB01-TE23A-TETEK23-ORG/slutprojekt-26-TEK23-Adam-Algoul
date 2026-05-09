1466bf3885db336526c6a88e50cde901875ec7cc
Adam algoul <waggdi.al-goul@elev.ga.ntig.se>
2026-05-10 00:53:08 +0200

    är typ klar med hela. har skrivt klart all html.css och java script

---
+++ java.js
@@ -0,0 +1,8 @@
+function visaText() {
+ let infoText = document.querySelector(".intressant-fakta").style.display ="block";
+ 
+    
+}
+function gomText() {
+    let infoText = document.querySelector(".intressant-fakta").style.display ="none";
+}
